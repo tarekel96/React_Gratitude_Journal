c9a9df6539e52c141d3a681f0fc5a034f83c0d85
Tarek <elhaj102@mail.chapman.edu>
2022-04-12 19:22:34 -0700

    changed default data

--- client/src/data/defaultData.js
+++ client/src/data/defaultData.js
@@ -1,5 +1,5 @@
 export const defaultData = [
 	{ content: 'I will pass my final exams!' },
 	{ content: 'I am artistic because I can paint like Van Gogh' },
-	{ content: 'I am smart' }
+	{ content: 'I will do well on my interview!' }
 ];
